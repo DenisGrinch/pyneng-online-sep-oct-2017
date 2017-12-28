d9c40da5659e86395f0dea5ca6e61804e68d2c8c
DenisGrinch <31757020+DenisGrinch@users.noreply.github.com>
2017-12-28 09:57:47 +0300

    Update task_11_1.py

--- exercises/11_db/task_11_1/task_11_1.py
+++ exercises/11_db/task_11_1/task_11_1.py
@@ -32,3 +32,65 @@
 На данном этапе, оба скрипта вызываются без аргументов.
 
 '''
+
+# Решение
+
+# Модуль создания базы
+
+import os
+import sqlite3
+
+
+schema_filename = 'dhcp_snooping_schema.sql'
+
+def createdb(dbname):
+    db_exists = os.path.exists(dbname)
+    if not db_exists:
+        print('Creating schema...')
+        conn = sqlite3.connect(dbname)
+        with open(schema_filename, 'r') as f:
+            schema = f.read()
+        conn.executescript(schema)
+        conn.close()
+        print('Done')
+    else:
+        print('Database exists, assume dhcp table does, too.')
+
+
+
+test = createdb('testbase.db')
+
+# Модуль добавления данных
+
+import sqlite3
+import re
+import glob
+
+dhcp_snoop_files = glob.glob('sw*_dhcp_snooping.txt')
+
+regex = re.compile('(\S+) +(\S+) +\d+ +\S+ +(\d+) +(\S+)')
+
+result = []
+
+for i in dhcp_snoop_files:
+    with open(i) as data:
+        for line in data:
+            match = regex.search(line)
+            if match:
+                result.append(match.groups())
+
+
+def adddata(datalist):
+    for row in datalist:
+        conn = sqlite3.connect('testbase.db')
+        try:
+            with conn:
+                query = '''insert into dhcp (mac, ip, vlan, interface)
+                           values (?, ?, ?, ?)'''
+                conn.execute(query, row)
+        except sqlite3.IntegrityError as e:
+            print('Error occured: ', e)
+    conn.close()
+
+test = adddata(result)
+
