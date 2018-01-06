1154f8d3a0fe7e8a40cdc3b9177a333e4cac4004
DenisGrinch <31757020+DenisGrinch@users.noreply.github.com>
2018-01-06 19:35:17 +0300

    key validation
    addes key vlaidation check

--- exercises/11_db/task_11_2a/task_11_2a.py
+++ exercises/11_db/task_11_2a/task_11_2a.py
@@ -20,3 +20,43 @@ $ python get_data_ver1.py vln 10
 
 '''
 
+# Решение
+
+import sqlite3
+import sys
+import pprint
+
+db_filename = 'testbase.db'
+
+conn = sqlite3.connect(db_filename)
+
+
+def getdata(*args):
+    if len(args) == 0:
+        with conn:
+            query = "select * from dhcp"
+            result = conn.execute(query)
+            for row in result:
+                pprint.pprint(row)
+    elif len(args) == 2:
+        conn.row_factory = sqlite3.Row
+        key, value = args
+        keys = ['mac', 'ip', 'vlan', 'interface']
+        if key in keys:
+            keys.remove(key)
+            print("\nDetailed information for host(s) with", key, value)
+            print('-' * 40)
+            with conn:
+                query = "select * from dhcp where {} = ?".format(key)
+                result = conn.execute(query, (value,))
+                for row in result:
+                    for k in keys:
+                        print('{:12}, {}'.format(k, row[k]))
+                        print('-' * 40)
+        else:
+            print('Valid keys are: ' + ", ".join(keys))
+    else:
+        print('Two or none parameters should be used')
+
+
+getdata(*sys.argv[1:])
