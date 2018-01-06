40896f5670d075a5a1ef21393c4109a00e7afc2b
DenisGrinch <31757020+DenisGrinch@users.noreply.github.com>
2018-01-06 08:53:56 +0300

    11_11а
    добавлен код с решением

--- exercises/11_db/task_11_1/add_data.py
+++ exercises/11_db/task_11_1/add_data.py
@@ -27,3 +27,44 @@ db_filename = 'dhcp_snooping.db'
 dhcp_snoop_files = glob.glob('sw*_dhcp_snooping.txt')
 #print(dhcp_snoop_files)
 
+
+# 11 и 11а сразу вставлю сюда. =) 
+
+
+import sqlite3
+import os
+import re
+import glob
+
+dhcp_snoop_files = glob.glob('sw*_dhcp_snooping.txt')
+
+regex = re.compile('(\S+) +(\S+) +\d+ +\S+ +(\d+) +(\S+)')
+
+dbname = input('please enter DB name: ')
+
+db_exists = os.path.exists(dbname)
+
+result = []
+
+for i in dhcp_snoop_files:
+    with open(i) as data:
+        for line in data:
+            match = regex.search(line)
+            if match:
+                result.append(match.groups())
+
+
+def adddata(datalist):
+    if not db_exists:
+        print('No database {0}. Need to create DB first'.format(dbname))
+    else:
+        for row in datalist:
+            conn = sqlite3.connect(dbname)
+            try:
+                with conn:
+                    query = '''insert into dhcp (mac, ip, vlan, interface)
+                                       values (?, ?, ?, ?)'''
+                    conn.execute(query, row)
+            except sqlite3.IntegrityError as e:
+                print('Error occured: ', e)
+            conn.close()
