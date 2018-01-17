60e048d1c2710aeaf14d55197339010cd1151fe5
DenisGrinch <31757020+DenisGrinch@users.noreply.github.com>
2018-01-17 09:21:24 +0300

    Решение задания 13.1

--- exercises/13_jinja2/task_13_1.py
+++ exercises/13_jinja2/task_13_1.py
@@ -30,3 +30,17 @@ template = env.get_template(template_file)
 vars_dict = yaml.load(open(VARS_FILE))
 
 print(template.render(vars_dict))
+
+
+#Решение 
+
+def config(path, yamlfile):
+    TEMPLATE_DIR, template_file = path.split('/')
+    VARS_FILE = yamlfile
+    env = Environment(loader = FileSystemLoader(TEMPLATE_DIR),
+                      trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template(template_file)
+
+    vars_dict = yaml.load(open(VARS_FILE))
+
+    print(template.render(vars_dict))
